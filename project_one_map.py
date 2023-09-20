8e651a7d4e8d17b4606d67b552479482dc43101c
wjparsle <111656744+wjparsle@users.noreply.github.com>
2023-09-20 09:14:58 -0500

    Add files via upload

---
+++ project_one_map.py
@@ -0,0 +1,14 @@
+#/usr/bin/python
+import pandas as pd
+from collections import defaultdict
+import sys
+
+df = pd.read_csv(sys.stdin)
+data = df['Activity'].dropna()
+year_count = defaultdict(int)
+for entry in data:
+    year_count[entry] += 1
+    
+for key in year_count.keys():
+    if year_count[key] > 3:
+        print(f'{key};{year_count[key]}')
---
+++ project_one_reduce.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+import sys
+
+total_counts = defaultdict(int)
+max_year = None
+max_count = -1
+for line in sys.stdin:
+    (year, count) = line.split(";")
+    count = int(count)
+    total_counts[year] += count
+    if count > max_count:
+        max_count = count
+        max_year = year
+print(f'{max_year}:{max_count}')
